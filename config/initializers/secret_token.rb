66984ca628b93564ac522999d76824341e0d24ca
q45 <quintinsmith@icloud.com>
2013-07-02 16:40:01 -0600

    rails for heroku rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = '79e477fe768089d60ae083167950ec363a35d4c31331876995aef429a6a12ec385ea0c1e457dcfcf3ef1ad4b671d9c270f216df0da7b8a30fd7b82cfd8608cdb'
